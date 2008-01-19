aa61e378bd51ea2b994b0843ea9007593eed8329
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2008-01-19 16:04:44 +0000

    Move renaming to yuidnd
    svn path=/kss.plugin.yuidnd/trunk/; revision=57196

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='kss.plugin.yuidnd',
+      version=version,
+      description="A plugin for KSS that provides drag-and-drop based on yahoo.ui",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='kss plugin drag-and-drop',
+      author='Jean-Paul Ladage',
+      author_email='j.ladage@zestsoftware.nl',
+      url='https://svn.plone.org/svn/collective/kss.plugin.yuidnd/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['kss.plugin'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
