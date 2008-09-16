3a02016c12df6cdebf15c23f9863894b867e728e
Reinout van Rees <reinout@vanrees.org>
2008-09-16 10:17:40 +0000

    Prepping release
    svn path=/kss.plugin.yuidnd/trunk/; revision=71910

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+import os
 
-version = '0.1'
+version = '0.7'
+descfile = open(os.path.join('kss', 'plugin', 'yuidnd', 'README.txt'))
+desc = descfile.read().strip()
+descfile.close()
+
 
 setup(name='kss.plugin.yuidnd',
       version=version,
       description="A plugin for KSS that provides drag-and-drop based on yahoo.ui",
-      long_description="""\
-""",
+      long_description=desc,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
