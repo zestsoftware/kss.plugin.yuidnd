5c535c69eeb7bf3d46f7c52806638a9ae1c4b5fa
Reinout van Rees <reinout@vanrees.org>
2008-09-16 10:18:48 +0000

    back to dev
    svn path=/kss.plugin.yuidnd/trunk/; revision=71912

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.7'
+version = '0.8 (svn/devel)'
 descfile = open(os.path.join('kss', 'plugin', 'yuidnd', 'README.txt'))
 desc = descfile.read().strip()
 descfile.close()
